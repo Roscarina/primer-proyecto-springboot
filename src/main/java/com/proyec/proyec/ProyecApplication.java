a3dd2c94cb793f53b997a57872dc88a595dd0993
Oscarina Romero <roscarina@gmail.com>
2024-11-29 17:58:48 -0500

    Primer commit

---
+++ src/main/java/com/proyec/proyec/ProyecApplication.java
@@ -0,0 +1,13 @@
+package com.proyec.proyec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyec/proyec/ProyecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyec.proyec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
