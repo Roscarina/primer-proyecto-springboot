5158c04112096fce4286ac2738875e69807baf46
Oscarina Romero <roscarina@gmail.com>
2024-11-29 18:28:20 -0500

    Datos_Productos

--- src/main/java/com/proyec/proyec/Model/Productos.java
+++ src/main/java/com/proyec/proyec/Model/Productos.java
@@ -1,5 +1,17 @@
 package com.proyec.proyec.Model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+
+
 public class Productos {
+
+    private int idProducto;
+    private String Nombre;
+    private int Valor;
+    private int Cantidad;
 
 }
